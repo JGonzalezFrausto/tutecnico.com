17775be6f369af97dc44649f82c730bac8c9dbd8
Samuel191 <samuel.moredi@gmail.com>
2019-10-10 18:59:55 -0500

    prueba fallida de geolocalizacion

--- prueba_localizacion.py
+++ prueba_localizacion.py
@@ -1,8 +1,16 @@
 from geopy.geocoders import Nominatim #geolocalizador 
+from geopy import distance
+
 
 geolocalizador = Nominatim()
 ubicacion = geolocalizador.reverse("21.1526503,-101.6777939")
 print(ubicacion.address)
 
+jardines_del_moral = ("21.1466363,-101.6924656")
+haciendas_del_rosario = ("21.1536054,-101.6529349")
+obregon = ("21.1322351,-101.6916145")
+san_jeronimo = ("21.1262232,-101.6733762")
+andrade = ("21.1146037,-101.6770974")
 
-from 
+print (distance.distance ( haciendas_del_rosario , jardines_del_moral ).km)
+print (distance.distance(obregon,san_jeronimo).m)
