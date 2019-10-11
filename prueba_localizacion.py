413c87572e994fbcdc37770c205b72f12932d755
Samuel191 <samuel.moredi@gmail.com>
2019-10-10 19:38:25 -0500

    obtencion de dirección, coordendas y distancias

--- prueba_localizacion.py
+++ prueba_localizacion.py
@@ -1,6 +1,11 @@
-from geopy.geocoders import Nominatim #geolocalizador 
+from geopy.geocoders import Nominatim #geolocalizador para obtener direcciones y latitud,longitud
 from geopy import distance
 
+geolocalizador = Nominatim() #dejar lista de argumentos vacia
+localizacion = geolocalizador.reverse ("21.1466363,-101.6924656")# con reverse se obtene la dirección apartir de coordenadas 
+colonia = geolocalizador.geocode ("Haciendas del Rosario") #obtiene coordenadas
+print (localizacion.address)
+print (( colonia.latitude, colonia.longitude))
 
 jardines_del_moral = ("21.1466363,-101.6924656")
 haciendas_del_rosario = ("21.1536054,-101.6529349")
@@ -9,4 +14,4 @@ san_jeronimo = ("21.1262232,-101.6733762")
 andrade = ("21.1146037,-101.6770974")
 
 print (distance.distance ( haciendas_del_rosario , jardines_del_moral ).km)
-print (distance.distance(obregon,san_jeronimo).m)
+print (distance.distance ( haciendas_del_rosario , jardines_del_moral ).m)
